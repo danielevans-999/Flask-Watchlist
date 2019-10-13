52f6d02c805a54890584344dc9ce72b58be0537e
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-13 07:40:41 +0300

    adds files

--- app/request.py
+++ app/request.py
@@ -1,11 +1,15 @@
 
-from app import app
 import urllib.request,json
-from .models import movie
-Movie = movie.Movie
+from .models import Movie
 
-api_key = app.config['MOVIE_API_KEY']
-base_url = app.config['MOVIE_API_BASE_URL']
+# Getting api key
+api_key = None
+# Getting the movie base url
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['MOVIE_API_BASE_URL']
 
 def get_movies(category):
     '''
@@ -52,8 +56,8 @@ def process_results(movie_list):
     return movie_results
 
 def get_movie(id):
-    get_movie_details_url = base_url.format(id,api_key)
-    with urllib.request.urlopen(get_movie_details_url) as url:
+    get_movie_url = base_url.format(id,api_key)
+    with urllib.request.urlopen(get_movie_url) as url:
         movie_details_data = url.read()
         movie_details_response = json.loads(movie_details_data)
         movie_object = None
@@ -61,12 +65,27 @@ def get_movie(id):
             id = movie_details_response.get('id')
             title = movie_details_response.get('original_title')
             overview = movie_details_response.get('overview')
-            poster = movie_details_response.get('poster')
+            poster = movie_details_response.get('poster_path')
             vote_average = movie_details_response.get('vote_average')
-            vote_count =  movie_details_response.get('vote_count')
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            vote_count = movie_details_response.get('vote_count')
             
-    return movie_object        
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+
+    return movie_object   
+
+def search_movie(movie_name):
+    search_movie_url =  'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+        search_movie_results = None
+        
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+    return search_movie_results  
+    
+         
         
     
                 
