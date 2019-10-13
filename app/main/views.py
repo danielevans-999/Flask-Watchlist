a8f6da1d14fea6200ec8290163f76e7c1eed8d1c
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-13 10:36:19 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -18,7 +18,7 @@ def index():
     title = 'Home - Welcome to The best Movie Review Website Online'
     search_movie = request.args.get('movie_query')
     if search_movie:
-        return redirect(url_for('.index'))
+        return redirect(url_for('main.search',movie_name = search_movie))
     else:
         return render_template('index.html',title = title,popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
@@ -53,7 +53,7 @@ def new_review(id):
         review = form.review.data
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
-        return redirect(url_for('.new_review' ))
+        return redirect(url_for('main.movie',id=movie.id ))
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 
- export MOVIE_API_KEY = '1a612bf729e479b9f236c64a3ddeb94b'
-export SECRET_KEY = '3ae7io3u'
+export MOVIE_API_KEY=1a612bf729e479b9f236c64a3ddeb94b
+export SECRET_KEY=3ae7io3u
 
 python3.6 manage.py server
